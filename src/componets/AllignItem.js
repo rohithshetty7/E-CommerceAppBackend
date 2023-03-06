233429156b4f8e81efbbbe32d4f6c8c0babddb35
rohithshetty7 <rohithshetty248@gmail.com>
2023-03-06 22:20:21 +0530

    pus

---
+++ src/componets/AllignItem.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AllignItem() {
+  return (
+    <div className="container c1">
+      <div >
+        <p>This is </p>
+      </div>
+    </div>
+  );
+}
+
+export default AllignItem;
